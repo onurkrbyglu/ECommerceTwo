5092de2f4497f6145ea42d87110d213ffd8e7891
Gürhan GEDİK <gurhan@faprika.com>
2020-05-01 21:01:44 +0300

    First Commit

---
+++ ECommerce/ECommerce.Web/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ECommerce.Web.Areas.Admin.Controllers
+{
+    public class CategoryController : Controller
+    {
+        GurhanDbEntities db = new GurhanDbEntities();
+       
+        // GET: Admin/Category
+        public ActionResult List()
+        {
+            
+            List<Category> cat = db.Categories.ToList();
+            return View(cat);
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Add(Category model)
+        {
+            Category cat = new Category();
+            cat.Name = model.Name;
+            cat.Published = model.Published;
+            cat.CreatedOnUtc = DateTime.Now;
+
+            db.Categories.Add(cat);
+            db.SaveChanges();
+
+            return RedirectToAction("List");
+        }
+
+
+        public ActionResult Edit(int id)
+        {
+            Category cat = db.Categories.SingleOrDefault(x => x.Id == id);
+            return View(cat);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Category model)
+        {
+            Category Category = db.Categories.SingleOrDefault(x => x.Id == model.Id);
+            Category.Name = model.Name;
+            Category.Published = model.Published;
+            Category.UpdatedOnUtc = DateTime.Now;
+            db.SaveChanges();
+           
+
+            return RedirectToAction("List");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            Category deleted = db.Categories.SingleOrDefault(x => x.Id == id);
+            db.Categories.Remove(deleted);
+            db.SaveChanges();
+            return RedirectToAction("List");
+        }
+    }
+}
---
+++ ECommerce/ECommerce.Web/Areas/Admin/Controllers/CustomerController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ECommerce.Web.Areas.Admin.Controllers
+{
+    public class CustomerController : Controller
+    {
+        GurhanDbEntities db = new GurhanDbEntities();
+
+        // GET: Admin/Category
+        public ActionResult List()
+        {
+
+            List<Customer> cat = db.Customers.ToList();
+            return View(cat);
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Add(Customer model)
+        {
+            model.CreatedOnUtc = DateTime.Now;
+            db.Customers.Add(model);
+            db.SaveChanges();
+
+            return RedirectToAction("List");
+        }
+
+
+        public ActionResult Edit(int id)
+        {
+            Customer cus = db.Customers.SingleOrDefault(x => x.Id == id);
+            return View(cus);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Customer model)
+        {
+            Customer customer = db.Customers.SingleOrDefault(x => x.Id == model.Id);
+            customer.FirstName = model.FirstName;
+            customer.LastName = model.LastName;
+            customer.Password = model.Password;
+            customer.Email = model.Email;
+            customer.Active = model.Active;
+            db.SaveChanges();
+
+
+            return RedirectToAction("List");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            Customer deleted = db.Customers.SingleOrDefault(x => x.Id == id);
+            db.Customers.Remove(deleted);
+            db.SaveChanges();
+            return RedirectToAction("List");
+        }
+    }
+}
